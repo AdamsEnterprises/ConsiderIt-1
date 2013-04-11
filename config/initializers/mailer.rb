df1257193525d7d439b78fa2602dfa2f1c5887d0
Travis Kriplean <tkriplean@gmail.com>
2013-04-11 01:21:11 -0700

    minor fix

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,4 @@
-has_aws = Rails.env.production? && APP_CONFIG.has_key? :aws && APP_CONFIG[:aws].has_key? :access_key_id && !APP_CONFIG[:aws][:access_key_id].nil?
+has_aws = Rails.env.production? && APP_CONFIG.has_key?(:aws) && APP_CONFIG[:aws].has_key?(:access_key_id) && !APP_CONFIG[:aws][:access_key_id].nil?
 
 if has_aws
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
