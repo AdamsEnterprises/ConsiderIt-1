96a5028d24177fd02829e9d772f3a42753ad4fd5
Travis Kriplean <tkriplean@gmail.com>
2013-04-11 01:23:45 -0700

    another minor fix

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -4,6 +4,7 @@ if has_aws
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
     :access_key_id     => APP_CONFIG[:aws][:access_key_id],
     :secret_access_key => APP_CONFIG[:aws][:secret_access_key]
+end
 
 Mailhopper::Base.setup do |config|
 
